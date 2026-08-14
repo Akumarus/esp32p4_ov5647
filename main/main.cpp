b36cd8fb07ea65206dc39b31a8a2366233b7b61b
Igor <strekalovskiy@uniscan.biz>
2026-08-14 13:02:12 +0700

    Add isp and defconfig

--- main/main.cpp
+++ main/main.cpp
@@ -10,6 +10,7 @@
 #include "esp_cam_sensor.h"
 #include "esp_cam_sensor_types.h"
 #include "ov5647.h" 
+#include "driver/isp.h"
 
 #include "esp_ldo_regulator.h"
 #include "esp_cam_ctlr.h"
@@ -18,29 +19,48 @@
 
 static QueueHandle_t s_frame_evt_queue;
 
+volatile uint32_t counter = 0;
+
 static bool IRAM_ATTR on_cam_trans_finished(esp_cam_ctlr_handle_t handle,
                                              esp_cam_ctlr_trans_t *trans,
                                              void *data)
 {
-    ESP_LOGI("MAIN", "CALLBACK");
-    BaseType_t hp_task_woken = pdFALSE;
-    xQueueSendFromISR(s_frame_evt_queue, &trans->buffer, &hp_task_woken);
-    return hp_task_woken == pdTRUE;
+    counter++;
+    return false;
 }
 
 static bool IRAM_ATTR on_cam_trans_started(esp_cam_ctlr_handle_t handle,
                                             esp_cam_ctlr_trans_t *trans,
                                             void *data)
 {
-    return pdTRUE;
+    esp_cam_ctlr_trans_t new_trans = *(esp_cam_ctlr_trans_t *)data;
+    trans->buffer = new_trans.buffer;
+    trans->buflen = new_trans.buflen;
+    return false;
 }
 
 
 extern "C" void app_main(void)
 {
     vTaskDelay(pdMS_TO_TICKS(1000));
-    
-    s_frame_evt_queue = xQueueCreate(2, sizeof(void *));
+
+    esp_ldo_channel_handle_t ldo_handler;
+    esp_ldo_channel_config_t ldo_cfg = {};
+    ldo_cfg.chan_id = 3;
+    ldo_cfg.voltage_mv = 2500;
+    ESP_ERROR_CHECK(esp_ldo_acquire_channel(&ldo_cfg, &ldo_handler));
+
+    uint32_t frame_size = 800 * 800 * 2;
+    void *frame_data = heap_caps_aligned_alloc(64, frame_size, MALLOC_CAP_SPIRAM | MALLOC_CAP_DMA);
+    if (frame_data == nullptr) {
+        ESP_LOGE("Main", "Не удалось выделить буффер размером %lu байт", frame_size);
+        return;
+    }
+
+    esp_cam_ctlr_trans_t trans = {
+        .buffer = frame_data,
+        .buflen = frame_size,
+    };
 
     i2c_master_bus_handle_t bus_handle;
     i2c_master_bus_config_t bus_cfg = {};
@@ -70,9 +90,11 @@ extern "C" void app_main(void)
     esp_cam_sensor_device_t *dev = ov5647_detect(&sensor_cfg);
     if (dev == NULL) {
         ESP_LOGI("Main", "OV5647 не обнаружен");
+        return;
     }
     ESP_LOGI("Main", "OV5647 обнаружен, name=%s", dev->name);
 
+    
     esp_cam_sensor_format_array_t formats = {};
     ESP_ERROR_CHECK(dev->ops->query_support_formats(dev, &formats));
 
@@ -91,58 +113,47 @@ extern "C" void app_main(void)
     ESP_ERROR_CHECK(dev->ops->set_format(dev, chosen));
     ESP_ERROR_CHECK(dev->ops->get_format(dev, &format));
 
+    int enable = 1;
+    dev->ops->priv_ioctl(dev, ESP_CAM_SENSOR_IOC_S_STREAM, &enable);
 
-    esp_ldo_channel_handle_t ldo_handler;
-    esp_ldo_channel_config_t ldo_cfg = {};
-    ldo_cfg.chan_id = 3;
-    ldo_cfg.voltage_mv = 2500;
-    ESP_ERROR_CHECK(esp_ldo_acquire_channel(&ldo_cfg, &ldo_handler));
-    
     esp_cam_ctlr_handle_t csi_handler = nullptr;
     esp_cam_ctlr_csi_config_t csi_cfg = {};
     csi_cfg.ctlr_id = 0;
-    csi_cfg.h_res = chosen->width;
-    csi_cfg.v_res = chosen->height;
-    csi_cfg.data_lane_num = 2;
+    csi_cfg.h_res = 800;
+    csi_cfg.v_res = 800;
+    csi_cfg.lane_bit_rate_mbps = 200;
     csi_cfg.input_data_color_type = CAM_CTLR_COLOR_RAW8;
-    csi_cfg.output_data_color_type = CAM_CTLR_COLOR_RAW8;
-    csi_cfg.lane_bit_rate_mbps = chosen->mipi_info.mipi_clk;
-    csi_cfg.queue_items = 2;
+    csi_cfg.output_data_color_type = CAM_CTLR_COLOR_RGB565;
+    csi_cfg.data_lane_num = 2;
+    csi_cfg.byte_swap_en = false;
+    csi_cfg.queue_items = 1;
     ESP_ERROR_CHECK(esp_cam_new_csi_ctlr(&csi_cfg, &csi_handler));
 
     esp_cam_ctlr_evt_cbs_t callbacks = {};
     callbacks.on_trans_finished = on_cam_trans_finished;
     callbacks.on_get_new_trans = on_cam_trans_started;
-
-    ESP_ERROR_CHECK(esp_cam_ctlr_register_event_callbacks(csi_handler, &callbacks, NULL));
-    
-    
-    uint32_t frame_size = chosen->width * chosen->height * 2;
-    void *frame_data = heap_caps_aligned_alloc(64, frame_size, MALLOC_CAP_SPIRAM | MALLOC_CAP_DMA);
-    if (frame_data == nullptr) {
-        ESP_LOGE("Main", "Не удалось выделить буффер размером %lu байт", frame_size);
-        return;
-    }
-
-    esp_cam_ctlr_trans_t trans = {};
-    trans.buffer = frame_data;
-    trans.buflen = frame_size;
-
-    ESP_ERROR_CHECK(esp_cam_ctlr_receive(csi_handler, &trans, ESP_CAM_CTLR_MAX_DELAY));
+    ESP_ERROR_CHECK(esp_cam_ctlr_register_event_callbacks(csi_handler, &callbacks, &trans));
 
     ESP_ERROR_CHECK(esp_cam_ctlr_enable(csi_handler));
-    ESP_ERROR_CHECK(esp_cam_ctlr_start(csi_handler));
-    int enable = 1;
-    dev->ops->priv_ioctl(dev, ESP_CAM_SENSOR_IOC_S_STREAM, &enable);
 
+    isp_proc_handle_t isp_handle = nullptr;
+    esp_isp_processor_cfg_t isp_cfg = {};
+    isp_cfg.clk_hz = 80000000;
+    isp_cfg.input_data_source = ISP_INPUT_DATA_SOURCE_CSI;
+    isp_cfg.input_data_color_type = ISP_COLOR_RAW8;
+    isp_cfg.output_data_color_type = ISP_COLOR_RGB565;
+    isp_cfg.has_line_start_packet = false;
+    isp_cfg.has_line_end_packet = false;
+    isp_cfg.h_res = 800;
+    isp_cfg.v_res = 800;
+    ESP_ERROR_CHECK(esp_isp_new_processor(&isp_cfg, &isp_handle));
+    ESP_ERROR_CHECK(esp_isp_enable(isp_handle));
+    ESP_ERROR_CHECK(esp_cam_ctlr_start(csi_handler));
+    ESP_ERROR_CHECK(esp_cam_ctlr_receive(csi_handler, &trans, ESP_CAM_CTLR_MAX_DELAY));
 
     while (true)
     {
-        void *ready_buf = NULL; 
-        if (xQueueReceive(s_frame_evt_queue, &ready_buf, pdMS_TO_TICKS(2000)) != pdTRUE) {
-            ESP_LOGW("Main", "Таймаут ожидания кадра — проверь физическое подключение "
-                          "CSI-шлейфа и питание сенсора");
-            continue;
-        }
+        ESP_LOGI("Main", "frame count = %lu", counter);
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
